bd00930e897e203ecd53b6016900288a784a850a
Theshakyhorse <amaximus.sierra@gmail.com>
2026-02-08 16:00:31 -0800

    second

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
 // COMSC-210 | Lab 4A | Alexander Sierra
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int main() {
+struct Color {
+    int red;
+    int green;
+    int blue;
+};
 
+int main() {
+    Color test;
+    test.red = 100;
+    test.green = 100;
+    test.blue = 100;
+    cout << test.red << " " << test.green << " " << test.blue << endl;
+    vector<Color> myVec;
+    myVec.push_back(test);
+    cout << myVec.at(0).red << " " << myVec.at(0).green << " " << myVec.at(0).blue << endl;
+    myVec.clear();
     return 0;
 }
