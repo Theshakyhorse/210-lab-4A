3f999b95c8b986cb2655050937d4fea503687556
Theshakyhorse <amaximus.sierra@gmail.com>
2026-02-08 16:49:23 -0800

    third

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+const int MN = 25, MX = 50;
 
 struct Color {
     int red;
@@ -10,14 +11,26 @@ struct Color {
 };
 
 int main() {
+    int n;
     Color test;
+    srand(time(0));
+
     test.red = 100;
     test.green = 100;
     test.blue = 100;
     cout << test.red << " " << test.green << " " << test.blue << endl;
+
     vector<Color> myVec;
     myVec.push_back(test);
     cout << myVec.at(0).red << " " << myVec.at(0).green << " " << myVec.at(0).blue << endl;
     myVec.clear();
+
+    n = rand() % (MX-MN+1) + MN;
+    for(int i = 1; i <= n; i++) {
+        Color temp;
+        temp.red = rand() % 255;
+        temp.green = rand() % 255;
+        temp.blue = rand() % 255;
+    }
     return 0;
 }
